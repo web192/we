5e8bb7758cb0ed1c76eeb8e7ab3dd0a76ae63c2c
web192 <josuevillatoro657@gmail.com>
2024-11-15 10:48:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', funtion(){
+  let Acerca de = this.document.getElementById('Acerca de')
+  let posicionObj1 = Acerca de.getBoundingClientRect().top()
+  console.log(posicionObj1)l;
+}
